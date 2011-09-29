3eccd3e17ba105469bcb8b174781ba63e5a95cf7
Glenn Pinto <gvpinto@gmail.com>
2011-09-28 22:48:09 -0400

    Changes to the configuration

---
+++ ParkFiftyTests/target/generated-sources/groovy-stubs/main/com/leopin/parkfifty/HelloMojo.java
@@ -0,0 +1,62 @@
+//
+// Generated stub from file:/Users/gvpinto/Git-Repos/leopin/ParkFiftyTests/src/main/groovy/com/leopin/parkfifty/HelloMojo.groovy
+//
+
+package com.leopin.parkfifty;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import org.codehaus.groovy.maven.mojo.GroovyMojo;
+
+/**
+ * Example Maven2 Groovy Mojo.
+ *
+ * @goal hello
+ */
+public class HelloMojo
+    extends GroovyMojo
+    implements groovy.lang.GroovyObject
+{
+    /**
+     * The hello message to display.
+     *
+     * @parameter expression="${message}" default-value="Hello World"
+     */
+    private java.lang.String message = null;
+    public java.lang.String getMessage() {
+        throw new InternalError("Stubbed method");
+    }
+    public void setMessage(java.lang.String value) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void execute() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public groovy.lang.MetaClass getMetaClass() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setMetaClass(groovy.lang.MetaClass metaClass) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object invokeMethod(java.lang.String name, java.lang.Object args) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object getProperty(java.lang.String name) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setProperty(java.lang.String name, java.lang.Object value) {
+        throw new InternalError("Stubbed method");
+    }
+}
