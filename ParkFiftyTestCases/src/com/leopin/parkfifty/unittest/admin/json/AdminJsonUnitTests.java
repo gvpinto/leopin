89b036a10a682e76735d5eb9b824d83b0fc404b5
Glenn Pinto <gvpinto@gmail.com>
2011-10-11 23:23:53 -0400

    Integration Testing with Spring

--- ParkFiftyTestCases/src/com/leopin/parkfifty/unittest/admin/json/AdminJsonUnitTests.java
+++ ParkFiftyTestCases/src/com/leopin/parkfifty/unittest/admin/json/AdminJsonUnitTests.java
@@ -12,75 +12,103 @@ import static org.mockito.Mockito.when;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
 
 import com.leopin.parkfifty.server.controller.AdminController;
 import com.leopin.parkfifty.server.service.AdminService;
 import com.leopin.parkfifty.shared.domain.Company;
 
-//@RunWith(SpringJUnit4ClassRunner.class)  
-//@ContextConfiguration(locations = { "classpath:parkfifty-servlet.xml" }) 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:parkfifty-servlet.xml" })
 public class AdminJsonUnitTests {
+
+	@Autowired
+	private ApplicationContext appCtx;
 
 	AdminService service = null;
 	MessageSource messages = null;
 	Company company = null;
-	
+
+	MockHttpServletRequest request;
+	MockHttpServletResponse response;
+	HandlerAdapter handlerAdapter;
+
 	@Before
 	public void setUp() throws Exception {
-		 service = mock(AdminService.class);
-		 messages = mock(MessageSource.class);
-		 company = mock(Company.class);
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+		service = mock(AdminService.class);
+		messages = mock(MessageSource.class);
+		company = mock(Company.class);
 	}
 
+	// @Test
+	// public void testRetrieveCompanyById() {
+	//
+	// when(company.getName()).thenReturn("Park Fifty");
+	//
+	// when(service.getCompany(anyLong())).thenReturn(company);
+	//
+	//
+	// AdminController controller = new AdminController(service, messages);
+	// assertEquals("Park Fifty", controller.getCompany("1").getName());
+	// verify(service, times(1)).getCompany(anyLong());
+	// verify(service, never()).getCompany(anyString());
+	//
+	// }
+	//
+	// @Test
+	// public void testRetrieveCompanyByName() {
+	//
+	// when(company.getName()).thenReturn("Park Fifty");
+	//
+	// when(service.getCompany(anyString())).thenReturn(company);
+	//
+	//
+	// AdminController controller = new AdminController(service, messages);
+	// assertEquals("Park Fifty",
+	// controller.getCompany("Park Fifty").getName());
+	// verify(service, never()).getCompany(anyLong());
+	// verify(service, times(1)).getCompany(anyString());
+	//
+	// }
+
+	// @Test
+	// public void addCompanyWithoutErrors() {
+	//
+	// when(company.getName()).thenReturn("Company without errors");
+	// when(company.getEmail()).thenReturn("gpinto@bbandt.com");
+	// when(company.getUrl()).thenReturn("http://www.bbt.com");
+	// when(company.getPriPhone()).thenReturn("9194553262");
+	//
+	// when(service.getCompany(anyString())).thenReturn(company);
+	//
+	//
+	// AdminController controller = new AdminController(service, messages);
+	// assertEquals("Park Fifty",
+	// controller.getCompany("Park Fifty").getName());
+	// verify(service, never()).getCompany(anyLong());
+	// verify(service, times(1)).getCompany(anyString());
+	//
+	// }
 
 	@Test
-	public void testRetrieveCompanyById() {
-		
-		when(company.getName()).thenReturn("Park Fifty");
-		
-		when(service.getCompany(anyLong())).thenReturn(company);
+	public void getCompany() throws Exception {
 
-		
-		AdminController controller = new AdminController(service, messages);
-		assertEquals("Park Fifty", controller.getCompany("1").getName());
-		verify(service, times(1)).getCompany(anyLong());
-		verify(service, never()).getCompany(anyString());
-		
+		handlerAdapter = appCtx.getBean(AnnotationMethodHandlerAdapter.class);
+		request.addHeader("Accept", "application/json");
+		request.setRequestURI("/admin/company/14");
+		request.setMethod("GET");
+		handlerAdapter.handle(request, response,
+				appCtx.getBean(AdminController.class));
+
 	}
-	
-	@Test
-	public void testRetrieveCompanyByName() {
-
-		when(company.getName()).thenReturn("Park Fifty");
-		
-		when(service.getCompany(anyString())).thenReturn(company);
-
-		
-		AdminController controller = new AdminController(service, messages);
-		assertEquals("Park Fifty", controller.getCompany("Park Fifty").getName());
-		verify(service, never()).getCompany(anyLong());
-		verify(service, times(1)).getCompany(anyString());
-		
-	}
-	
-	@Test
-	public void addCompanyWithoutErrors() {
-
-		when(company.getName()).thenReturn("Company without errors");
-		when(company.getEmail()).thenReturn("gpinto@bbandt.com");
-		when(company.getUrl()).thenReturn("http://www.bbt.com");
-		when(company.getPriPhone()).thenReturn("9194553262");
-		
-		when(service.getCompany(anyString())).thenReturn(company);
-
-		
-		AdminController controller = new AdminController(service, messages);
-		assertEquals("Park Fifty", controller.getCompany("Park Fifty").getName());
-		verify(service, never()).getCompany(anyLong());
-		verify(service, times(1)).getCompany(anyString());
-		
-	}	
 }
