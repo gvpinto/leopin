9c1f2ffe50f82dc2e71ab9ab64df7ce5deaca506
Glenn Pinto <gvpinto@gmail.com>
2012-01-30 08:05:03 -0500

    Daily Commit

--- ParkFifty/src/com/leopin/parkfifty/client/ui/CompanyWidget.java
+++ ParkFifty/src/com/leopin/parkfifty/client/ui/CompanyWidget.java
@@ -1,21 +1,21 @@
 package com.leopin.parkfifty.client.ui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.DomEvent;
-import com.google.gwt.event.dom.client.HasKeyPressHandlers;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyPressEvent;
 import com.google.gwt.event.dom.client.KeyPressHandler;
-import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.leopin.parkfifty.shared.utils.Utils;
 
 public class CompanyWidget extends Composite implements KeyPressHandler {
 
@@ -33,6 +33,24 @@ public class CompanyWidget extends Composite implements KeyPressHandler {
 	// Had to rename the button field as continue is a key fiels
 	@UiField
 	Button uiContinue;
+	
+	@UiField
+	TextBox uiName;
+	
+	@UiField
+	TextBox uiUrl;
+	
+	@UiField
+	TextBox uiEmail;
+	
+	@UiField
+	TextBox uiPriPhone;
+	
+	@UiField
+	TextBox uiSecPhone;
+	
+	@UiField
+	TextBox uiFax;
 
 	@UiHandler("uiContinue")
 	void onClick(ClickEvent e) {
@@ -43,6 +61,13 @@ public class CompanyWidget extends Composite implements KeyPressHandler {
 	public void onKeyPress(KeyPressEvent event) {
 		if (event.getUnicodeCharCode() == KeyCodes.KEY_ENTER)
 			Window.alert("Enter Key Pressed");
+	}
+	
+	@UiHandler("uiPriPhone")
+	public void formatPhone(BlurEvent event) {
+		
+		NumberFormat.getFormat("(###) ###-###0").format(Utils.scrubPhoneNum(uiPriPhone)); 
+		
 	}
 
 }
